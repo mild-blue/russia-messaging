344d8782645fca06380b840363283d0091e0cacb
Tomáš Kouřim <kourim@outlook.com>
2022-03-01 15:51:45 +0100

    readme

--- settings_example.py
+++ settings_example.py
@@ -1,2 +1,3 @@
 WEBDRIVER_PATH = "C:\\Users\\path\\to\\chromedriver.exe"
+# "You are at war. Putin has attacked Ukraine, murdering women and children. Russian soldiers are dying. Totally unnecessary. Please, help stop this maddness."
 MESSAGE = "Идет война. Путин напал на Украину. Он убивает женщин и детей. Погибают российские солдаты. Абсолютно без причины. Пожалуйста, помогите остановить это безумие."
